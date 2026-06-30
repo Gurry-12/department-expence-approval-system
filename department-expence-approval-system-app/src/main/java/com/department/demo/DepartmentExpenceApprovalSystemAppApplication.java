75bb1a73959ee5f806bce5afce14183fc2c63ee1
Gurry-12 <96852785+Gurry-12@users.noreply.github.com>
2026-06-30 15:30:11 +0530

    chore: initialized sprong boot project

---
+++ department-expence-approval-system-app/src/main/java/com/department/demo/DepartmentExpenceApprovalSystemAppApplication.java
@@ -0,0 +1,13 @@
+package com.department.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepartmentExpenceApprovalSystemAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepartmentExpenceApprovalSystemAppApplication.class, args);
+	}
+
+}
